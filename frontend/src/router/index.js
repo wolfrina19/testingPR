cfea48a405c32a3bc93181f4aea4fadb6fd1a5e8
Rime-seria <seriarime@gmail.com>
2026-04-20 23:24:09 +0100

    Initialisation de la partie front de la feature RequestAccess

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import RequestAccessView from '../views/RequestAccessView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,6 +13,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/request-access',
+      name: 'request-access',
+      component: RequestAccessView,
     },
   ],
 })
