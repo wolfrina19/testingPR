366009d1bbfef2deba09f202d56b8a11e9a0a71b
Rime-seria <seriarime@gmail.com>
2026-05-04 16:23:38 +0100

    feat: gestion des apis

--- frontend/src/services/requestAccessService.js
+++ frontend/src/services/requestAccessService.js
@@ -1,8 +1,6 @@
 import api from './api'
 
 export const requestAccess = async (payload) => {
-  // À confirmer avec le backend :
-  // endpoint exact + noms exacts des champs
-  const response = await api.post('/access-requests', payload)
+  const response = await api.post('/access-requests', payload) //a verifier avec back
   return response.data
 }
